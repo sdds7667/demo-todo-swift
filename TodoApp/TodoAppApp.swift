284abbbf7287c8929e0b8cdab28e9e747e6b1278
sdds7667 <inplmd@gmail.com>
2023-06-06 12:03:49 +0200

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Ion Plamadeala on 06/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
